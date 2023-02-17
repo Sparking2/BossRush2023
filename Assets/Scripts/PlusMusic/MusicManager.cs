32a9f761ee0112ad80fb09c80325370fc234f7e0
Sparking2 <sparking02@yahoo.com.mx>
2023-02-17 00:44:20 -0600

    PlusMusic

---
+++ Assets/Scripts/PlusMusic/MusicManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using UnityEngine;
+
+namespace PlusMusic
+{
+    public class MusicManager : MonoBehaviour
+    {
+        // private void Start()
+        // {
+        //     StartCoroutine(nameof( ChangeSong ));
+        // }
+        //
+        // private IEnumerator ChangeSong()
+        // {
+        //     yield return new WaitForSeconds(15.0f);
+        //     Debug.Log("CHANGING SOUNDTRACK");
+        //    PlusMusic_DJ.Instance.PlayArrangement(new TransitionInfo(PlusMusic_DJ.PMTags.victory));
+        // }
+    }
+}
